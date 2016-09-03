2651a6b6ee71c3824d640000e11c2c234ab412c1
Alex Povar <zvirja1@gmail.com>
2016-09-03 13:23:04 +0300

    Add ThreadStack

---
+++ CodeBricks.Tests/ThreadStackTests.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Threading;
+using FluentAssertions;
+using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+
+namespace CodeBricks.Tests
+{
+  public class ThreadStackTests
+  {
+    [Theory]
+    [AutoData]
+    public void PushedElementCanBeRead(object element)
+    {
+      try
+      {
+        //act
+        ThreadStack<object>.PushValue(element);
+
+        //assert
+        ThreadStack<object>.Current.Should().Be(element);
+      }
+      finally
+      {
+        ThreadStack<object>.PopValue();
+      }
+    }
+
+    [Theory]
+    [AutoData]
+    public void LastPushedElementIsReturned(object element1, object element2)
+    {
+      try
+      {
+        //arrange
+        ThreadStack<object>.PushValue(element1);
+
+        //act
+        ThreadStack<object>.PushValue(element2);
+
+        //assert
+        ThreadStack<object>.Current.Should().Be(element2);
+      }
+      finally
+      {
+        ThreadStack<object>.PopValue();
+        ThreadStack<object>.PopValue();
+      }
+    }
+
+    [Theory]
+    [AutoData]
+    public void StackDistinguishTypes(object objElement, string strElement)
+    {
+      try
+      {
+        //act
+        ThreadStack<object>.PushValue(objElement);
+        ThreadStack<string>.PushValue(strElement);
+
+        //assert
+        ThreadStack<string>.Current.Should().Be(strElement);
+      }
+      finally
+      {
+        ThreadStack<object>.PopValue();
+        ThreadStack<string>.PopValue();
+      }
+    }
+
+    [Theory]
+    [AutoData]
+    public void PopWorksCorrectly(object element)
+    {
+      //arrange
+      ThreadStack<object>.PushValue(element);
+
+      //act
+      ThreadStack<object>.PopValue();
+
+      //assert
+      ThreadStack<object>.Current.Should().BeNull();
+    }
+
+    [Theory]
+    [AutoData]
+    public void ScopeWorksCorrectly(object element)
+    {
+      //act & assert
+      using (ThreadStack<object>.EnterScope(element))
+      {
+        ThreadStack<object>.Current.Should().Be(element);
+      }
+
+      ThreadStack<object>.Current.Should().BeNull();
+    }
+
+    [Theory]
+    [AutoData]
+    public void StackIsThreadBound(object elementThr1, object elementThr2)
+    {
+      try
+      {
+        //arrange
+        ThreadStack<object>.PushValue(elementThr1);
+
+        var otherThreadInvoked = false;
+        object otherThreadCaptured = null;
+
+        var thread = new Thread(() =>
+        {
+          otherThreadInvoked = true;
+          otherThreadCaptured = ThreadStack<object>.Current;
+
+          ThreadStack<object>.PushValue(elementThr2);
+        });
+
+        //act
+        thread.Start();
+        thread.Join();
+
+        //assert
+        otherThreadInvoked.Should().BeTrue();
+        otherThreadCaptured.Should().BeNull();
+        ThreadStack<object>.Current.Should().Be(elementThr1);
+      }
+      finally
+      {
+        ThreadStack<object>.PopValue();
+      }
+    }
+
+    [Fact]
+    public void CurrentDoesntFailIfStackIsNull()
+    {
+      //act
+      Action getter = () => ThreadStack<object>.GetCurrentValue();
+
+      //assert
+      getter.ShouldNotThrow();
+    }
+
+    [Fact]
+    public void PopDoesntThrowIfNotAsserted()
+    {
+      //act
+      Action pop = () => ThreadStack<object>.PopValue(false);
+
+      //assert
+      pop.ShouldNotThrow();
+    }
+
+    [Fact]
+    public void PopThrowsIfAsserted()
+    {
+      //act
+      Action pop = () => ThreadStack<object>.PopValue();
+
+      //assert
+      pop.ShouldThrow<InvalidOperationException>();
+    }
+  }
+}
---
+++ CodeBricks/ThreadStack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeBricks
+{
+  public static class ThreadStack<TElementType>
+  {
+    [ThreadStatic] private static Stack<TElementType> _stack;
+
+    private static readonly IDisposable DisposableCleaner = new StackCleaner();
+
+    public static TElementType Current => GetCurrentValue();
+
+    public static TElementType GetCurrentValue(TElementType defValue = default(TElementType))
+    {
+      if ((_stack == null) || (_stack.Count == 0)) return defValue;
+
+      return _stack.Peek();
+    }
+
+    public static void PushValue(TElementType value)
+    {
+      if (_stack == null) _stack = new Stack<TElementType>();
+
+      _stack.Push(value);
+    }
+
+    public static void PopValue(bool assertStackContainValue = true)
+    {
+      if ((_stack == null) || (_stack.Count == 0))
+      {
+        if (assertStackContainValue) throw new InvalidOperationException("Thread stack is empty. Unable to pop value.");
+        return;
+      }
+
+      _stack.Pop();
+    }
+
+    public static IDisposable EnterScope(TElementType value)
+    {
+      PushValue(value);
+      return DisposableCleaner;
+    }
+
+    private class StackCleaner : IDisposable
+    {
+      public void Dispose() => PopValue();
+    }
+  }
+}
