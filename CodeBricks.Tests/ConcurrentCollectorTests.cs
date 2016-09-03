e82c93008ef1625a543b4fbbe13a90b6b1e1b963
Alex Povar <zvirja1@gmail.com>
2016-09-03 13:32:46 +0300

    Add concurrent collector

---
+++ CodeBricks.Tests/ConcurrentCollectorTests.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+
+namespace CodeBricks.Tests
+{
+  public class ConcurrentCollectorTests
+  {
+    private static void PutDataConcurrently<T>(ConcurrentCollector<T> collector, int threads, int numElementsPerThread, Func<int, T> factory)
+    {
+      var tasks = new Task[threads];
+
+      for (var i = 0; i < threads; i++)
+        tasks[i] = Task.Run(() =>
+        {
+          for (var j = 0; j < numElementsPerThread; j++)
+            collector.Add(factory(j));
+        });
+
+      Task.WaitAll(tasks);
+    }
+
+    [Theory]
+    [AutoData]
+    public void EnumeratorReturnsSameNumberAsCountIfLessThanCapacity(int elementsPerThread)
+    {
+      //arrange
+      var sut = new ConcurrentCollector<int>(10000);
+      PutDataConcurrently(sut, 4, elementsPerThread%2000 + 1, x => x);
+      var allElementsCount = sut.Count;
+
+      //act
+      var enumerableCount = sut.Count();
+
+      //assert
+      enumerableCount.Should().Be(allElementsCount);
+    }
+
+    [Theory]
+    [AutoData]
+    public void EnumeratorReturnsSameNumberAsCountIfMoreThanCapacity(int randomCapacity, int elementsPerThread)
+    {
+      //arrange
+      var capacity = randomCapacity%10 + 1;
+      var sut = new ConcurrentCollector<int>(capacity);
+      PutDataConcurrently(sut, Environment.ProcessorCount, elementsPerThread%1000 + capacity, x => x);
+      var allElementsCount = sut.Count;
+
+      //act
+      var enumerableCount = sut.Count();
+
+      //assert
+      enumerableCount.Should().Be(allElementsCount);
+    }
+
+    [Fact]
+    public void CollectorRespectsOrderInSigleThreadedPopulation()
+    {
+      //arrange
+      var entries = Enumerable.Range(150, 1000).ToArray();
+      var sut = new ConcurrentCollector<int>(10);
+
+      //act
+      PutDataConcurrently(sut, 1, 1000, x => entries[x]);
+
+      //assert
+      sut.SequenceEqual(entries).Should().BeTrue();
+    }
+
+    [Fact]
+    public void CountIsCorrectIfCapacityIsGreaterThanElementsNumber()
+    {
+      //arrange
+      var sut = new ConcurrentCollector<int>(10000);
+      PutDataConcurrently(sut, 4, 1000, x => x);
+
+      //act
+      var actualCount = sut.Count;
+
+      //assert
+      actualCount.Should().Be(4000);
+    }
+
+    [Fact]
+    public void CountIsCorrectIfCapacityIsLessThanElementsNumber()
+    {
+      //arrange
+      var sut = new ConcurrentCollector<int>(10);
+      PutDataConcurrently(sut, 4, 10000, x => x);
+
+      //act
+      var actualCount = sut.Count;
+
+      //assert
+      actualCount.Should().Be(40000);
+    }
+
+    [Fact]
+    public void EnumeratorReturnsAllEntries()
+    {
+      //arrange
+      var entries = Enumerable.Range(0, 1000);
+      var queue = new ConcurrentQueue<int>(entries);
+
+      var sut = new ConcurrentCollector<int>(100);
+
+      //act
+      PutDataConcurrently(sut, 4, 250, x =>
+      {
+        int res;
+        var success = queue.TryDequeue(out res);
+
+        success.Should().BeTrue();
+        return res;
+      });
+
+      //assert
+      sut.Should().BeEquivalentTo(entries);
+    }
+
+    [Fact]
+    public void WritesDontFailForHugeConcurrency()
+    {
+      //arrange
+      const int ConcurrencyLevel = 1000;
+      const int ItemsPerThread = 100000;
+      var sut = new ConcurrentCollector<int>(9);
+
+      //act
+      Action concurrentPopulation = () =>
+      {
+        var threads = new Thread[ConcurrencyLevel].ToList();
+
+        for (var i = 0; i < ConcurrencyLevel; i++)
+          threads[i] = new Thread(() =>
+          {
+            for (var j = 0; j < ItemsPerThread; ++j)
+              sut.Add(j);
+          });
+
+        threads.ForEach(x => x.Start());
+        threads.ForEach(x => x.Join());
+      };
+
+      //assert
+      concurrentPopulation.ShouldNotThrow();
+      sut.Count.Should().Be(ConcurrencyLevel*ItemsPerThread);
+    }
+  }
+}
---
+++ CodeBricks/ConcurrentCollector.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace CodeBricks
+{
+  /// <summary>
+  ///   Collection that implements Multiple Producers - No Consumers scenario.
+  ///   <para>
+  ///     Common usage is to collect data in parallel and later pass it to consumer.
+  ///     Current collection supports multiple writers, however reads should be performed only after all the writers finished
+  ///     their work.
+  ///     Reads could be parallel as well.
+  ///   </para>
+  /// </summary>
+  public class ConcurrentCollector<T> : IEnumerable<T>
+  {
+    private readonly int _chunkCapacity;
+
+    private readonly object _syncRoot = new object();
+
+    public ConcurrentCollector(int chunkCapacity)
+    {
+      if (chunkCapacity <= 0)
+        throw new ArgumentOutOfRangeException(nameof(chunkCapacity));
+
+      this._chunkCapacity = chunkCapacity;
+      this.Chunks = new List<Chunk<T>>();
+
+      this.AddNewChunkUnsafe();
+    }
+
+    private List<Chunk<T>> Chunks { get; }
+
+    private Chunk<T> CurrentChunk { get; set; }
+
+    /// <summary>
+    ///   Returns number of elements in collector. Note, is not valid during collection phase, should only be used afterwards.
+    /// </summary>
+    public int Count
+    {
+      get
+      {
+        //All chunks except the last one are full. Chunks count is always greater than zero. The last chunk is CurrentChunk
+        var fullChunksCount = this._chunkCapacity*(this.Chunks.Count - 1);
+        var lastChunkCount = this.CurrentChunk.ActualLength;
+
+        return fullChunksCount + lastChunkCount;
+      }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+      //We just merge all the values returned by chunks
+      return this.Chunks.SelectMany(x => x).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+      return this.GetEnumerator();
+    }
+
+
+    /// <summary>
+    ///   Adds element to collector. Is thread safe.
+    /// </summary>
+    public void Add(T value)
+    {
+      while (!this.CurrentChunk.TryAdd(value))
+        this.EnsureCurrentChunkNotFullSync();
+    }
+
+    private void EnsureCurrentChunkNotFullSync()
+    {
+      lock (this._syncRoot)
+      {
+        if (this.CurrentChunk.IsFull)
+          this.AddNewChunkUnsafe();
+      }
+    }
+
+    private void AddNewChunkUnsafe()
+    {
+      var newChunk = new Chunk<T>(this._chunkCapacity);
+      this.Chunks.Add(newChunk);
+      this.CurrentChunk = newChunk;
+    }
+
+    private class Chunk<TElement> : IEnumerable<TElement>
+    {
+      private readonly int _capacity;
+      private readonly TElement[] _data;
+
+      private volatile int _lastElementIndex;
+
+      public Chunk(int capacity)
+      {
+        this._data = new TElement[capacity];
+        this._capacity = capacity;
+        this._lastElementIndex = -1;
+      }
+
+      /// <summary>
+      ///   Checks whether
+      /// </summary>
+      public bool IsFull
+      {
+        get { return this._lastElementIndex >= this._capacity - 1; }
+      }
+
+      /// <summary>
+      ///   Returns actual capaticy. Note, it ins't expected to return valid value during the population phase.
+      /// </summary>
+      public int ActualLength
+      {
+        get { return this._lastElementIndex >= this._capacity ? this._capacity : this._lastElementIndex + 1; }
+      }
+
+      public IEnumerator<TElement> GetEnumerator()
+      {
+        if (this.IsFull)
+          return this._data.AsEnumerable().GetEnumerator();
+
+        //If chunk not empty - return inserted values only
+        return this._data.Take(this._lastElementIndex + 1).GetEnumerator();
+      }
+
+      IEnumerator IEnumerable.GetEnumerator()
+      {
+        return this.GetEnumerator();
+      }
+
+      public bool TryAdd(TElement element)
+      {
+        //Ensure that chunk is not already full
+        if (this.IsFull)
+          return false;
+
+        var newIndex = Interlocked.Increment(ref this._lastElementIndex);
+
+        //Check whether new index is still in range. If not - current chunk is full
+        if (newIndex >= this._capacity)
+        {
+          //Useless a bit, however idea is to ensure that this._index is not overflowed very much in case of huge concurrency
+          this._lastElementIndex = this._capacity;
+          return false;
+        }
+
+        this._data[newIndex] = element;
+        return true;
+      }
+    }
+  }
+}
